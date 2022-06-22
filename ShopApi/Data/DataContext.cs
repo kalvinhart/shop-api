668730b6e0eef3fd7c3cb21b4981a3f60e0f592b
Kalvin <kalvinhart@hotmail.co.uk>
2022-06-22 13:01:15 +0100

    Added DataContext.

---
+++ ShopApi/Data/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShopApi.Models;
+
+namespace ShopApi.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; } = null!;
+        public DbSet<Product> Products { get; set; } = null!;
+    }
+}
