3ad4303261d3237eca889f3fa5884b9c4374c309
Kalvin <kalvinhart@hotmail.co.uk>
2022-06-22 12:18:01 +0100

    Added Category model.

---
+++ ShopApi/Models/Category.cs
@@ -0,0 +1,15 @@
+﻿namespace ShopApi.Models
+{
+    public class Category
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = null!;
+        public List<Subcategory> Subcategories { get; set; } = new List<Subcategory> { };
+
+        public class Subcategory
+        {
+            public int Id { get; set; }
+            public string Name { get; set; } = null!;
+        }
+    }
+}
