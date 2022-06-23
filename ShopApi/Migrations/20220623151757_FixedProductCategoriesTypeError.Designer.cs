68a4c256bdd99fe3e1359018c94e6fac69c226c7
Kalvin <kalvinhart@hotmail.co.uk>
2022-06-23 20:28:01 +0100

    New migration after fix.

--- ShopApi/Migrations/20220623151757_FixedProductCategoriesTypeError.Designer.cs
+++ ShopApi/Migrations/20220623151757_FixedProductCategoriesTypeError.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ShopApi.Data;
 
@@ -11,9 +12,10 @@ using ShopApi.Data;
 namespace ShopApi.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20220623151757_FixedProductCategoriesTypeError")]
+    partial class FixedProductCategoriesTypeError
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ ShopApi/Migrations/20220623151757_FixedProductCategoriesTypeError.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ShopApi.Migrations
+{
+    public partial class FixedProductCategoriesTypeError : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Categories_Products_ProductId",
+                table: "Categories");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Categories_ProductId",
+                table: "Categories");
+
+            migrationBuilder.DropColumn(
+                name: "ProductId",
+                table: "Categories");
+
+            migrationBuilder.CreateTable(
+                name: "ProductCategory",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ProductId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductCategory", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ProductCategory_Products_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Products",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductCategory_ProductId",
+                table: "ProductCategory",
+                column: "ProductId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ProductCategory");
+
+            migrationBuilder.AddColumn<int>(
+                name: "ProductId",
+                table: "Categories",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Categories_ProductId",
+                table: "Categories",
+                column: "ProductId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Categories_Products_ProductId",
+                table: "Categories",
+                column: "ProductId",
+                principalTable: "Products",
+                principalColumn: "Id");
+        }
+    }
+}
