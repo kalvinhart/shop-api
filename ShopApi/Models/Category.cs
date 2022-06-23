38118d20a060dab5a9aa634a3bde524347301ae5
Kalvin <kalvinhart@hotmail.co.uk>
2022-06-23 15:39:16 +0100

    Refactored Id to int.

--- ShopApi/Models/Category.cs
+++ ShopApi/Models/Category.cs
@@ -2,13 +2,14 @@
 {
     public class Category
     {
-        public Guid Id { get; set; }
+        public int Id { get; set; }
         public string Name { get; set; } = null!;
         public List<Subcategory> Subcategories { get; set; } = new List<Subcategory> { };
 
         public class Subcategory
         {
             public int Id { get; set; }
+            public int CategoryId { get; set; }
             public string Name { get; set; } = null!;
         }
     }
