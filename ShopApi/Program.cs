a40d2c19688b1d1e3f4d82a65aec15260ca46b93
Kalvin <kalvinhart@hotmail.co.uk>
2022-06-23 20:59:43 +0100

    Added Automapper.

--- ShopApi/Program.cs
+++ ShopApi/Program.cs
@@ -1,11 +1,13 @@
 using Microsoft.EntityFrameworkCore;
 using ShopApi.Data;
+using ShopApi.RequestHelpers;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddAutoMapper(typeof(MappingProfiles).Assembly);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
---
+++ ShopApi/RequestHelpers/MappingProfiles.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using ShopApi.DTOs;
+using ShopApi.Models;
+
+namespace ShopApi.RequestHelpers
+{
+    public class MappingProfiles : Profile
+    {
+        public MappingProfiles()
+        {
+            CreateMap<EditProductDto, Product>();
+        }
+    }
+}
